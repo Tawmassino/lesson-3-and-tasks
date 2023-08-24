258540190953aad77632175bb509f0023a98424e
Tawmassino <etomas815@gmail.com>
2023-08-24 18:22:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,303 @@
+﻿// See https://aka.ms/new-console-template for more information
+//pastabos: readline visada paima string kad ir kas bebutu, tada ji reikia konvertuoti i kazka kita
+// pavadinimai turi buti visada unikalus ir nedubliuotis, net jei ir kitas tipas.
+
+
+
+//conditional tasks
+
+//3 uzduotis apie password
+#region
+//Console.WriteLine("Iveskite slaptazodi");
+//string userInput = Console.ReadLine()
+
+//if (userInput == "srekasforever") { Console.WriteLine("Sekmingai prisijungete"); }
+//else if (userInput == "Mellon") { Console.WriteLine("Sekmingai prisijungete"); }
+//else if (userInput == "01101001 01101110") { Console.WriteLine("Nulaužta..“. "); }
+//else { Console.WriteLine("Slaptažodis neteisingas, prašome bandyti dar kartą.."); }
+
+#endregion
+
+
+
+//3 uzduotis apie atsibudima
+#region
+//Console.WriteLine("Kelinta valanda siandien pabudote?");
+
+//string userInput = Console.ReadLine();
+//int userInputInteger = Convert.ToInt32(userInput);
+
+//if (userInputInteger < 12) { Console.WriteLine("Labas rytas!"); }
+//else if (userInputInteger >=12 && userInputInteger < 18) { Console.WriteLine("Geros popietes!"); }
+//else if (userInputInteger >18 && userInputInteger < 24) { Console.WriteLine("Gero vakaro!"); }
+
+#endregion
+
+//2 uzduotis apie dalinima is 5
+#region
+//Console.WriteLine("Iveskite bet koki skaiciu");
+//string userInput = Console.ReadLine();
+//int userInputInteger = Convert.ToInt32(userInput);
+
+//if (userInputInteger %5 == 0 ) { Console.WriteLine("Skaicius gali buti dalintas is 5"); }
+//else { Console.WriteLine("Skaicius negali buti dalintas is 5"); }
+
+
+#endregion
+
+//2 uzduotis apie temperatura
+#region
+//Console.WriteLine("Kiek laipsniu rodo termometras siuo metu?");
+
+//string userInput = Console.ReadLine();
+//int userInputInteger = Convert.ToInt32(userInput);
+
+//if (userInputInteger <= 0) { Console.WriteLine("Šalta"); }
+//else if (userInputInteger > 0) 
+//{    Console.WriteLine("Vėsu");         }
+//else if (userInputInteger > 20) { Console.WriteLine("Karšta"); }
+
+//else { Console.WriteLine("Kažkas negerai"); }
+
+#endregion
+
+//1 uzduotis apie dienos numeri
+#region
+//Console.WriteLine("Iveskite savaites dienos skaiciu");
+
+//string userInput = Console.ReadLine();
+//int userInputInteger = Convert.ToInt32(userInput);//nereikia nes cia ir stringo uztenka//visgi reikia
+
+//if (userInputInteger == 1){
+//    Console.WriteLine("Jusu diena yra pirmadienis" );
+//}
+//else if (userInputInteger == 2)
+//{
+//    Console.WriteLine("Jusu diena yra antradienis");
+//}
+//else if (userInputInteger == 3)
+//{
+//    Console.WriteLine("Jusu diena yra treciadienis");
+//}
+//else if (userInputInteger == 4)
+//{
+//    Console.WriteLine("Jusu diena yra ketvirtadienis");
+//}
+//else if (userInputInteger == 5)
+//{
+//    Console.WriteLine("Jusu diena yra penktadienis");
+//}
+//else if (userInputInteger == 6)
+//{
+//    Console.WriteLine("Jusu diena yra sestadienis");
+//}
+//else if (userInputInteger == 7)
+//{
+//    Console.WriteLine("Jusu diena yra sekmadienis");
+//}
+//else
+//    Console.WriteLine( "Neteisingas dienos numeris");
+
+//Console.ReadKey(true);
+//Console.WriteLine("Dekojame uz dalyvavima");
+
+#endregion
+
+//1 uzduotis apie dienos numeri bet daryt ne su integer, o su string
+#region
+//Console.WriteLine("Iveskite savaites dienos skaiciu");
+
+//string userInput = Console.ReadLine();
+////int userInputInteger = Convert.ToInt32(userInput);
+
+//if (userInput == "1")
+//{
+//    Console.WriteLine("Jusu diena yra pirmadienis");
+//}
+//else if (userInput == "2")
+//{
+//    Console.WriteLine("Jusu diena yra antradienis");
+//}
+//else if (userInput == "3")
+//{
+//    Console.WriteLine("Jusu diena yra treciadienis");
+//}
+//else if (userInput == "4")
+//{
+//    Console.WriteLine("Jusu diena yra ketvirtadienis");
+//}
+//else if (userInput == "5")
+//{
+//    Console.WriteLine("Jusu diena yra penktadienis");
+//}
+//else if (userInput == "6")
+//{
+//    Console.WriteLine("Jusu diena yra sestadienis");
+//}
+//else if (userInput == "7")
+//{
+//    Console.WriteLine("Jusu diena yra sekmadienis");
+//}
+//else
+//    Console.WriteLine("Neteisingas dienos numeris");
+
+//Console.ReadKey(true);
+//Console.WriteLine("Dekojame uz dalyvavima");
+
+#endregion
+
+
+//1 uzduotis apie 100
+#region
+//Console.WriteLine("Iveskite skaiciu");
+//string userInput = Console.ReadLine();
+//int userInputInteger = Convert.ToInt32(userInput);
+
+//if  (userInputInteger > 100)
+//{
+//    Console.WriteLine("Jusu skaicius yra didesnis nei 100"); 
+//}
+//else if (userInputInteger == 100)
+//{
+//    Console.WriteLine("Jusu skaicius yra 100");
+//}
+//else if (userInputInteger < 100)
+//    Console.WriteLine("Jusu skaicius yra mazesnis nei 100");
+
+//Console.ReadKey(true);
+//Console.WriteLine("Dekojame uz dalyvavima");
+#endregion
+
+
+
+
+
+//conditional
+
+#region;
+//bool myTrueVariable = true;
+//bool myFalseVariable = false;
+
+
+//int number = 5;
+//if(number > 0) 
+//{
+//    Console.WriteLine("skaicius yra teigiamas");
+//}
+//else if(number <0)
+//{
+//    Console.WriteLine("skaicius yra neigiamas");
+//}
+
+
+//int zalgiris = 92;
+//int lietuvosRytas = 95;
+
+//if(zalgiris > lietuvosRytas)
+//{
+//    Console.WriteLine("laimejo zalgiris");
+//}
+//else if (zalgiris < lietuvosRytas)
+//{
+//    Console.WriteLine("laimejo lietuvos rytas");
+//}
+
+//else //nereikia salygos su ELSE
+//{
+//    Console.WriteLine("lygiosios");
+//}
+
+//Console.WriteLine();
+
+
+//pirmas conditional dalykas
+
+//int age = 18;
+//if (age == 18)
+//{
+//        Console.WriteLine("Jus esate 18 metu amziaus");
+//}
+
+//int number = 10;
+//if (number != 0)  //not equal condition
+//{ Console.WriteLine("Skaicius nelygus 0"); }
+
+//int score = 90;
+//if (score < 90)
+//{
+//    Console.WriteLine("Jusu rezultatas yra mazesnis nei 90");
+//}
+
+
+#endregion;
+
+
+//uzduotis
+#region
+
+//string testWord = "    kiskiakopustis   ";
+//Console.WriteLine(testWord);
+//Console.WriteLine(testWord.Substring(6));
+
+//string testWord2 = testWord.Replace('k', 'K');
+//Console.WriteLine(testWord2);
+
+//Console.WriteLine(testWord.IndexOf("a"));
+
+
+//Console.WriteLine(testWord.Trim());
+
+
+//Console.WriteLine(testWord.ToUpperInvariant());
+//Console.WriteLine(testWord.ToLowerInvariant());
+
+
+
+Console.WriteLine();
+
+Console.WriteLine();
+Console.WriteLine();
+#endregion
+
+//toUpperInvariant
+# region = toUpper;
+//string someText = "Sveiki, kaip jums sekasi?";
+//Console.WriteLine(someText.ToUpperInvariant()); //nepamirsti skliaustu uzdeti
+
+//string test = "Hello my friend";
+//string hello = test.Substring(0, 5);
+//string friend = test.Substring(8, 7); //antras skaicius nurodo KIEK, o ne koordinates
+//Console.WriteLine(hello);
+//Console.WriteLine(friend);
+#endregion;
+
+
+//+=
+#region; 
+
+//string word1 = "ap";
+//string word2 = "gal";
+//string word3 = "voti";
+
+//string result = ""; //reikia butinai kad butu kabutes su stringais.
+//result += word1; //ap
+//result += word2; //gal
+//result += word3; //voti
+
+////sudeda juos visus i viena
+
+#endregion
+
+
+//Console.WriteLine(result);
+
+//string result = word1 + word2 + word3;
+//Console.WriteLine(result);
+
+//trim
+#region;
+//string word1 = "  Buonasera, Maddona!      ";
+//Console.WriteLine(word1);
+//string word2 = word1.Trim();
+//Console.WriteLine(word2);
+#endregion
